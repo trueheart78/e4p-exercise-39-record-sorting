4932a7745ac861c046b6382f0503375ad1503bd7
Josh Mills <josh@trueheart78.com>
2015-12-10 22:50:44 -0500

    Model spec for employees for validation

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject(:employee) { FactoryGirl.create :employee }
+
+  context 'validation checks' do
+
+    it 'works like the happiness we love' do
+      expect(subject).to be_valid
+    end
+
+    it 'requires the first_name' do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'requires the last_name' do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'requires the position' do
+      subject.position = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
+
 end
