150fab7767aadea8f7c4c9b4f21663eec717d238
Josh Mills <josh@trueheart78.com>
2015-12-13 20:40:30 -0500

    Employees controller now supports sorting records

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,24 @@
 class EmployeesController < ApplicationController
+  SORTABLE_FIELDS = ['last_name',
+                     'first_name',
+                     'position',
+                     'separation_date']
   def index
-    @employees = Employee.all
+    @employees = Employee.all.order sort_field
   end
+
+  private
+
+  def sort_params
+    params.permit :sort_field
+  end
+
+  def sort_field
+    if SORTABLE_FIELDS.include? sort_params[:sort_field]
+      sort_params[:sort_field]
+    else
+      :last_name
+    end
+  end
+
 end
